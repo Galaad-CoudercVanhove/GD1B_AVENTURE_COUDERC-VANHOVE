182daa08ae77b4931fa7e6017fe7b27b51041ddf
Galaad-CoudercVanhove <74669543+Galaad-CoudercVanhove@users.noreply.github.com>
2021-04-20 16:34:38 +0200

    Create Scene1.js

---
+++ Scene1.js
@@ -0,0 +1,51 @@
+var player;
+var cursors;
+var wallLayer;
+
+class Scene1 extends Phaser.Scene{
+    constructor(){
+        super("Scene1");
+    }
+    init(data){
+    }
+    preload(){   
+        this.load.tilemapTiledJSON('map', 'map.json');
+        this.load.image('tiles', 'assets/landscape_tiles.png');
+        this.load.image('player', 'assets/star.png');
+    }
+    create(){
+        const map = this.make.tilemap({key: 'map'});
+        const tileset = map.addTilesetImage('landscape', 'tiles');
+        
+        map.createStaticLayer('ground', tileset, 0, 0);
+        wallLayer = map.createStaticLayer('mur', tileset, 0, 0);
+
+        wallLayer.setCollisionByExclusion(-1, true);
+
+    /*    player = this.physics.add.sprite(300, 300, 'player');
+
+        this.physics.add.collider(player, mur);*/
+
+        cursors = this.input.keyboard.createCursorKeys();
+        
+
+    }
+    
+    update(){
+        if (cursors.right.isDown){
+            player.setVelocityX(200);
+        }
+        else if (cursors.left.isDown){
+            player.setVelocityX(-200);
+        }
+        else if (cursors.up.isDown){
+            player.setVelocityY(-200);
+        }
+        else if (cursors.down.isDown){
+            player.setVelocityY(200);
+        }
+        else{
+            player.setVelocity(0);
+        }
+    }
+}
